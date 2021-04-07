f2480f4cc9b087eebfba6fb4b37a65d640b10a6c
Alex Delgado <gadc1996@gmail.com>
2021-04-07 13:25:57 -0600

    Add create article

--- cali/articles.py
+++ cali/articles.py
@@ -6,7 +6,7 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from cali.lib.db import get_db
-from cali.lib.article import Article, get_all_articles, get_filtered_articles, get_single_article, article_exist
+from cali.lib.article import Article, get_all_articles, get_filtered_articles, get_single_article
 from cali.lib.category import get_single_category, get_all_categories
 
 blueprint = Blueprint('articles', __name__, url_prefix='/articles')
@@ -31,14 +31,15 @@ def update(id):
     if request.method == 'POST':
         db = get_db()
         article = Article(get_single_article(id))
+        flash(article.article_exist())
 
-        if article_exist(article):
+        if article.article_exist():
             g.message = 'article Exists'
             g.messageColor = 'danger'
         else:
             g.message = 'article Updated'
             g.messageColor = 'success'
-            db.execute(article.update_article(id))
+            db.execute(article.update_article())
             db.commit()
     else:
         article = Article(get_single_article(id))
@@ -52,23 +53,28 @@ def create():
         db = get_db()
         article = Article(request.form)
 
-        if article_exist(article):
+        if article.article_exist():
             g.message = 'article Exists'
             g.messageColor = 'danger'
         else:
             g.message = 'article Created'
             g.messageColor = 'success'
+            flash(article.create_article())
             db.execute(article.create_article())
             db.commit()
 
-    return render_template('articles/create.html')
+    else:
+        article = Article(get_single_article(1))
+
+    categories = get_all_categories()
+    return render_template('articles/create.html', categories=categories)
 
 
 @blueprint.route('/<int:id>/delete', methods=('GET',))
 def delete(id):
     db = get_db()
     article = Article(get_single_article(id))
-    db.execute(article.delete_article(id))
+    db.execute(article.delete_article())
     db.commit()
     return redirect(url_for('articles.search'))
 
--- cali/lib/article.py
+++ cali/lib/article.py
@@ -5,9 +5,8 @@ class Article:
     """ A simple article class """
 
     def __init__(self, iterable):
-        self.id = self._is_valid(iterable['id'], 'id')
         self.name = self._is_valid(iterable['name'], 'name')
-        #self.category = self._is_valid(iterable['category'], 'category')
+        self.id = self.get_article_id()
         self.description = self._is_valid(iterable['description'], 'description')
         self.price = self._is_valid(iterable['price'], 'price')
         self.sku = self._is_valid(iterable['sku'], 'sku')
@@ -15,7 +14,8 @@ class Article:
         self.on_branch_1 = self._is_valid(iterable['on_branch_1'], 'on_branch_1')
         self.on_branch_2 = self._is_valid(iterable['on_branch_2'], 'on_branch_2')
         self.is_regular = self._is_valid(iterable['is_regular'], 'is_regular')
-        self.category = get_single_category(iterable['category_id'])
+        self.category_id = self._is_valid(iterable['category_id'], 'category_id')
+        self.category = get_single_category(self.category_id)
 
     def _is_valid(self, field, fieldName):
         if field is None:
@@ -24,10 +24,10 @@ class Article:
         return field
 
     def create_article(self):
-        return "INSERT INTO article (name, category_id, description, price, SKU, stock, on_branch_1, on_branch_2, is_regular" \
-        f"VALUES( {self.name}, {self.category}, {self.description}, {self.price}, {self.sku}, {self.stock}, {self.on_branch_1}, {self.on_branch_2}, {self.is_regular})"
+        return "INSERT INTO article (name, category_id, description, price, SKU, stock, on_branch_1, on_branch_2, is_regular) " \
+        f"VALUES ('{self.name}', {self.category_id}, '{self.description}', {self.price}, {self.sku}, {self.stock}, {self.on_branch_1}, {self.on_branch_2}, {self.is_regular})"
 
-    def update_article(self, id):
+    def update_article(self):
         return 'UPDATE article '\
             f'SET name="{self.name}" ,  '\
             f'category_id=1 , '\
@@ -36,10 +36,27 @@ class Article:
             f'on_branch_1={self.on_branch_1} , '\
             f'on_branch_2={self.on_branch_2} , '\
             f'is_regular={self.is_regular} '\
-            f'WHERE id={id} '
+            f'WHERE id={self.id} '
 
-    def delete_article(self, id):
-     return f'DELETE FROM article WHERE article.id={id}'
+    def delete_article(self):
+     return f'DELETE FROM article WHERE article.id={self.id}'
+
+    def get_article_id(self):
+        db = get_db()
+        article_id = db.execute(f"SELECT id FROM article WHERE name='{self.name}'").fetchone()
+        if article_id is not None:
+            return article_id['id']
+        else:
+            
+            article_id = db.execute(f"SELECT id FROM article").fetchall()
+            return article_id[-1]['id'] + 1
+
+    def article_exist(self):
+        db = get_db()
+        if db.execute(f"SELECT name FROM article WHERE name='{self.name}'").fetchone() is not None:
+            return True
+        else:
+            return False
 
 def get_single_article(id):
     db = get_db()
@@ -71,9 +88,3 @@ def get_filtered_articles(form):
                 ).fetchall()
             return articles
 
-def article_exist(client):
-    db = get_db()
-    if db.execute(f"SELECT name FROM client WHERE name='{client.name}'").fetchone() is not None:
-        return True
-    else:
-        return False
